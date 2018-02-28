9db3274ad92534260f91d6f351db666b2284b0c5
body <body@bode>
2018-03-01 01:43:59 +0200

    productDao

--- src/main/java/com/apicompany/e/commerceapplication/dal/dao/daoimpl/ProductDAO.java
+++ src/main/java/com/apicompany/e/commerceapplication/dal/dao/daoimpl/ProductDAO.java
@@ -6,11 +6,40 @@
 package com.apicompany.e.commerceapplication.dal.dao.daoimpl;
 
 import com.apicompany.e.commerceapplication.dal.dao.daoint.ProductDAOInt;
+import com.apicompany.e.commerceapplication.dal.models.Product;
+import java.util.ArrayList;
 
 /**
  *
  * @author Vargos
  */
 public class ProductDAO implements ProductDAOInt {
-    
+
+    @Override
+    public ArrayList<Product> getallProducts() {
+        return null;
+    }
+
+    @Override
+    public boolean insertProduct(Product newProduct) {
+      return true;
+     
+    }
+
+    @Override
+    public boolean deleteProduct(Product currentProduct) {
+      return true; 
+    }
+
+    @Override
+    public boolean updateProduct(Product currentproduct) {
+       return true;
+    }
+
+    @Override
+    public Product getSpecificProduct(int ProductId) {
+       return null; 
+    }
+
+   
 }
