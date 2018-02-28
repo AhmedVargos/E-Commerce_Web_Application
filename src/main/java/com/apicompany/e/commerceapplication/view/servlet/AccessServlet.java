4623310895ed4c1e7c2910eba0eb97db1b249e32
Sherif93 <sherifzakaria@outlook.com>
2018-02-28 18:52:11 +0200

    Access Servlet Fix

--- src/main/java/com/apicompany/e/commerceapplication/view/servlet/AccessServlet.java
+++ src/main/java/com/apicompany/e/commerceapplication/view/servlet/AccessServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "AccessServlet")
+@WebServlet(name = "AccessServlet", urlPatterns = {"/AccessServlet"})
 public class AccessServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
